b8a081cd93332bb2d504dc01031e6e4e307250c2
YuliaPortnova <yuyul@yandex.ru>
2023-04-19 15:57:27 +0300

    Исправляет ошибки в js

--- source/js/index.js
+++ source/js/index.js
@@ -1,6 +1,6 @@
 const mainHeaderToggle = document.querySelector('.main-header__toggle');
 const mainНeader = document.querySelector('.main-header');
 
-mainHeaderToggle.eddEventListener('click', () => {
-  mainНeader.classList.toggle('is-open');
-});
+mainНeader.classList.remove('no-js');
+
+mainHeaderToggle.addEventListener('click', () => mainНeader.classList.toggle('is-open'));
