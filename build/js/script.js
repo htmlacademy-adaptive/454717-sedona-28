3ea0c5e17b53e5433c5c517b7772809a32733f1e
YuliaPortnova <yuyul@yandex.ru>
2023-05-11 13:53:53 +0300

    Удаляет папку build

--- build/js/script.js
+++
@@ -1,1 +0,0 @@
-const mainHeaderToggle=document.querySelector(".main-header__toggle"),mainНeader=document.querySelector(".main-header");mainНeader.classList.remove("no-js"),mainHeaderToggle.addEventListener("click",(()=>mainНeader.classList.toggle("is-open")));
